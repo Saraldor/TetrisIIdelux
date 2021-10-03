9280a6502719321a67ab955ae4db5886b518f756
christer Holm <ch@star78.se>
2021-10-03 13:37:02 +0200

    draw line

--- src/Board.java
+++ src/Board.java
@@ -4,7 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Board extends JPanel {
-   private Timer looper;
+   public static final int BOARD_WIDTH= 10;
+    public static final int BOARD_HEIGHT= 20;
+    public static final int BLOCK_SIZE= 30;
+
+    private Timer looper;
+   private Color [][] board = new  Color[BOARD_WIDTH][BOARD_HEIGHT];
+
     public Board(){
         looper = new Timer(500, new ActionListener() {
 int n = 0;
@@ -23,8 +29,18 @@ System.out.println(n++);
 
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+g.setColor(Color.black);
+g.fillRect(0,0,getWidth(),getHeight());
+        g.setColor(Color.white);
+        for (int row =0; row < BOARD_HEIGHT; row++){
+            g.drawLine(0,BLOCK_SIZE*row,BLOCK_SIZE*BOARD_WIDTH,BLOCK_SIZE*row  );
+        }
+        for (int col =0; col < BOARD_WIDTH+ 1; col++){
+            g.drawLine(col*BLOCK_SIZE,0,col*BLOCK_SIZE,BLOCK_SIZE* BOARD_HEIGHT  );
+        }
 
 
-        g.drawRect(10,10,200,200);
+
+
     }
 }
