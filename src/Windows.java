b4109fb09340cacb0d02fa824a8d68083d5fb925
christer Holm <ch@star78.se>
2021-10-03 13:05:32 +0200

    some new future

--- src/Windows.java
+++ src/Windows.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 
 public class Windows {
     public static final int WIDTH = 445, HEIGHT = 629;
+   private Board board;
     private JFrame window;
     public Windows() {
         window = new JFrame("Tetris II Delux");
 window.setSize(WIDTH, HEIGHT);
 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 window.setResizable(false);
-
-
+window.setLocationRelativeTo(null);
+board = new Board();
+window.add(board);
 window.setVisible(true);
     }
     public static void main(String[]args){
